c86d30856eab3922a72fb31ad110e4d72c1ef121
Washani <maneshawashani@yahoo.com>
2024-09-11 22:23:49 +0530

    Update AppDelegate.mm

--- ios/FindLocations/AppDelegate.mm
+++ ios/FindLocations/AppDelegate.mm
@@ -7,7 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyDeGuVPSjNeVaqZ_zxODqZeTPEekOnXRYA"];
+  [GMSServices provideAPIKey:@"GOOGLE_MAPS_APIKEY"];
   self.moduleName = @"FindLocations";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
